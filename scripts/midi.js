ded2b8af0a15140943deee0d51fd096717578e79
unknown <Bert.DeSaffel@Ugent.be>
2020-04-13 18:49:24 +0200

    refactor project and allow pitch bend range

--- scripts/midi.js
+++ scripts/midi.js
@@ -1,82 +1,90 @@
-var audioContext;
-var oscillator;
-var gain;
-var FREQUENCIES = [8.18, 8.66, 9.18, 9.72, 10.3, 10.91, 11.56, 12.25, 12.98, 13.75, 14.57, 15.43, 16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87, 32.7, 34.65, 36.71, 38.89, 41.2, 43.65, 46.25, 49, 51.91, 55, 58.27, 61.74, 65.41, 69.3, 73.42, 77.78, 82.41, 87.31, 92.5, 98, 103.83, 110, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185, 196, 207.65, 220, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392, 415.30, 440, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 830.61, 880, 932.33, 987.77, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53, 2093, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040, 7458.62, 7902.13, 8372.02, 8869.84, 9397.27, 9956.06, 10548.08, 11175.3, 11839.82, 12543.85, 13289.75];
-var NOTE_ON_EVENT = 144;
-var NOTE_OFF_EVENT = 128;
-var PITCH_BEND = 224;
-var MOD_BEND = 176;
-var notesArray;
-var selectBox = document.getElementById('midi-select');
-var midiInputPorts = [];
-var selectedMidiInputPort;
-window.onload = function () {
-    navigator.requestMIDIAccess()
-        .then(requestMIDIAccessResolve, requestMIDIAccessReject);
-    selectBox.onchange = selectMIDIInputPort;
-};
-function requestMIDIAccessResolve(midiAccess) {
-    var midiInputs = midiAccess.inputs;
-    midiInputs.forEach(function (input) {
-        midiInputPorts.push(input);
-    });
-    updateMIDISelectBox();
-}
-function requestMIDIAccessReject(exception) {
-    alert(exception);
-}
-function updateMIDISelectBox() {
-    selectBox.innerHTML = '';
-    var option = document.createElement('option');
-    option.innerHTML = '----------';
-    selectBox.appendChild(option);
-    for (var i = 0; i < midiInputPorts.length; i++) {
-        var inputPort = midiInputPorts[i];
-        var option_1 = document.createElement('option');
-        option_1.value = i.toString();
-        option_1.innerHTML = inputPort.name;
-        selectBox.appendChild(option_1);
+'use strict';
+var VirtualPiano = (function () {
+    function VirtualPiano() {
+        var _this = this;
+        this.selectMIDIDeviceBox = document.getElementById('midi-select');
+        this.selectBendRangeBox = document.getElementById('midi-bend-range');
+        this.bendRange = 2 * 128;
+        this.midiInputPorts = [];
+        this.FREQUENCIES = [8.18, 8.66, 9.18, 9.72, 10.3, 10.91, 11.56, 12.25, 12.98, 13.75, 14.57, 15.43, 16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87, 32.7, 34.65, 36.71, 38.89, 41.2, 43.65, 46.25, 49, 51.91, 55, 58.27, 61.74, 65.41, 69.3, 73.42, 77.78, 82.41, 87.31, 92.5, 98, 103.83, 110, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185, 196, 207.65, 220, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392, 415.30, 440, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 830.61, 880, 932.33, 987.77, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53, 2093, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040, 7458.62, 7902.13, 8372.02, 8869.84, 9397.27, 9956.06, 10548.08, 11175.3, 11839.82, 12543.85, 13289.75];
+        navigator.requestMIDIAccess()
+            .then(function (midiAccess) { _this.requestMIDIAccessResolve(midiAccess); }, function (exception) { return _this.requestMIDIAccessReject(exception); });
     }
-    selectBox.selectedIndex = 0;
-}
-function selectMIDIInputPort() {
-    audioContext = new AudioContext();
-    oscillator = audioContext.createOscillator();
-    oscillator.type = "sawtooth";
-    oscillator.start(0);
-    selectedMidiInputPort = midiInputPorts[selectBox.selectedIndex - 1];
-    selectedMidiInputPort.onmidimessage = function (e) {
+    VirtualPiano.prototype.requestMIDIAccessResolve = function (midiAccess) {
+        var _this = this;
+        var midiInputs = midiAccess.inputs;
+        midiInputs.forEach(function (input) {
+            _this.midiInputPorts.push(input);
+        });
+        this.updateMIDISelectBox();
+        this.selectMIDIDeviceBox.onchange = function () { return _this.selectMIDIInputPort(); };
+        this.selectBendRangeBox.onchange = function () { return _this.setBendRange(); };
+    };
+    VirtualPiano.prototype.requestMIDIAccessReject = function (exception) {
+        alert(exception);
+    };
+    VirtualPiano.prototype.setBendRange = function () {
+        this.bendRange = parseInt(this.selectBendRangeBox.value) * 128;
+    };
+    VirtualPiano.prototype.selectMIDIInputPort = function () {
+        var _this = this;
+        console.log(this.audioContext);
+        if (this.audioContext == null) {
+            this.audioContext = new AudioContext();
+            this.oscillator = this.audioContext.createOscillator();
+            this.oscillator.start(0);
+        }
+        this.selectedMIDIInputPort = this.midiInputPorts[this.selectMIDIDeviceBox.value];
+        console.log(this.selectMIDIDeviceBox.selectedIndex);
+        this.selectedMIDIInputPort.onmidimessage = function (e) { return _this.processMIDIMessage(e); };
+    };
+    VirtualPiano.prototype.processMIDIMessage = function (e) {
         var data = e.data;
         var channel = data[0] & 0xF;
         var command = data[0] & ~0xF;
         switch (command) {
             case 1:
-                setModulation(data[2]);
+                this.setModulation(data[2]);
                 break;
             case 0x80:
-                noteOff(data[1], data[2]);
+                this.noteOff(data[1], data[2]);
                 break;
             case 0x90:
-                noteOn(data[1], data[2]);
+                this.noteOn(data[1], data[2]);
                 break;
             case 0xE0:
-                setBend(data[1], data[2]);
+                this.setBend(data[1], data[2]);
                 break;
         }
     };
-}
-function noteOn(note, force) {
-    oscillator.frequency.setTargetAtTime(FREQUENCIES[note], audioContext.currentTime, 0);
-    oscillator.connect(audioContext.destination);
-}
-function noteOff(note, force) {
-    oscillator.disconnect(audioContext.destination);
-}
-function setBend(highNibble, lowNibble) {
-    var combination = (lowNibble << 7) | highNibble;
-    var bend = (combination - 8192) * (256 * 100 / 127) / 8192;
-    oscillator.detune.setValueAtTime(bend, audioContext.currentTime);
-}
-function setModulation(value) {
-    gain.gain.setValueAtTime(value * 100 / 127, audioContext.currentTime);
-}
+    VirtualPiano.prototype.noteOn = function (note, force) {
+        this.oscillator.frequency.setTargetAtTime(this.FREQUENCIES[note], this.audioContext.currentTime, 0);
+        this.oscillator.connect(this.audioContext.destination);
+    };
+    VirtualPiano.prototype.noteOff = function (note, force) {
+        this.oscillator.disconnect(this.audioContext.destination);
+    };
+    VirtualPiano.prototype.setBend = function (highNibble, lowNibble) {
+        var combination = (lowNibble << 7) | highNibble;
+        var bend = (combination - 8192) * (this.bendRange * 100 / 127) / 8192;
+        this.oscillator.detune.setValueAtTime(bend, this.audioContext.currentTime);
+    };
+    VirtualPiano.prototype.setModulation = function (value) {
+    };
+    VirtualPiano.prototype.updateMIDISelectBox = function () {
+        this.selectMIDIDeviceBox.innerHTML = '';
+        var option = document.createElement('option');
+        option.innerHTML = '----------';
+        this.selectMIDIDeviceBox.appendChild(option);
+        for (var i = 0; i < this.midiInputPorts.length; i++) {
+            var inputPort = this.midiInputPorts[i];
+            var option_1 = document.createElement('option');
+            option_1.value = i.toString();
+            option_1.innerHTML = inputPort.name;
+            this.selectMIDIDeviceBox.appendChild(option_1);
+        }
+        this.selectMIDIDeviceBox.selectedIndex = 0;
+    };
+    return VirtualPiano;
+}());
+new VirtualPiano();
--- scripts/midi.ts
+++ scripts/midi.ts
@@ -1,102 +1,116 @@
-let audioContext: AudioContext;
-let oscillator: OscillatorNode;
-let gain: GainNode;
-
-const FREQUENCIES = [8.18, 8.66, 9.18, 9.72, 10.3, 10.91, 11.56, 12.25, 12.98, 13.75, 14.57, 15.43, 16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87, 32.7, 34.65, 36.71, 38.89, 41.2, 43.65, 46.25, 49, 51.91, 55, 58.27, 61.74, 65.41, 69.3, 73.42, 77.78, 82.41, 87.31, 92.5, 98, 103.83, 110, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185, 196, 207.65, 220, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392, 415.30, 440, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 830.61, 880, 932.33, 987.77, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53, 2093, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040, 7458.62, 7902.13, 8372.02, 8869.84, 9397.27, 9956.06, 10548.08, 11175.3, 11839.82, 12543.85, 13289.75]
+'use strict';
 
 
-const NOTE_ON_EVENT = 144;
-const NOTE_OFF_EVENT = 128;
-const PITCH_BEND = 224;
-const MOD_BEND = 176;
+class VirtualPiano {
+    /* HTML BINDINGS */
+    private selectMIDIDeviceBox = <HTMLSelectElement>document.getElementById('midi-select');
+    private selectBendRangeBox = <HTMLSelectElement>document.getElementById('midi-bend-range');
 
-let notesArray;
+    private bendRange = 2 * 128; // 2 semitones
 
+    private midiInputPorts: WebMidi.MIDIInput[] = [];
+    private selectedMIDIInputPort: WebMidi.MIDIInput;
 
-const selectBox = <HTMLSelectElement>document.getElementById('midi-select');
+    private audioContext: AudioContext;
+    private oscillator: OscillatorNode;
 
-let midiInputPorts: WebMidi.MIDIInput[] = [];
-let selectedMidiInputPort: WebMidi.MIDIInput;
-
-
-/* https://www.w3.org/TR/webmidi/ */
-window.onload = function () {
-    /* https://www.w3.org/TR/webmidi/#requestmidiaccess */
-    navigator.requestMIDIAccess()
-        .then(requestMIDIAccessResolve, requestMIDIAccessReject);
-
-    selectBox.onchange = selectMIDIInputPort;
-}
-
-/* https://www.w3.org/TR/webmidi/#midiaccess-interface */
-function requestMIDIAccessResolve(midiAccess: WebMidi.MIDIAccess) {
-    let midiInputs: WebMidi.MIDIInputMap = midiAccess.inputs;
-    midiInputs.forEach(function (input: WebMidi.MIDIInput) {
-        midiInputPorts.push(input);
-    });
-    updateMIDISelectBox();
-}
-
-function requestMIDIAccessReject(exception: any) {
-    alert(exception);
-}
+    private FREQUENCIES = [8.18, 8.66, 9.18, 9.72, 10.3, 10.91, 11.56, 12.25, 12.98, 13.75, 14.57, 15.43, 16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87, 32.7, 34.65, 36.71, 38.89, 41.2, 43.65, 46.25, 49, 51.91, 55, 58.27, 61.74, 65.41, 69.3, 73.42, 77.78, 82.41, 87.31, 92.5, 98, 103.83, 110, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185, 196, 207.65, 220, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392, 415.30, 440, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 830.61, 880, 932.33, 987.77, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53, 2093, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040, 7458.62, 7902.13, 8372.02, 8869.84, 9397.27, 9956.06, 10548.08, 11175.3, 11839.82, 12543.85, 13289.75]
 
 
-/**
- * The user can select which MIDI input device to use.
- * This function should be called when the state of the MIDIAccess object changes.
- */
-function updateMIDISelectBox() {
-    selectBox.innerHTML = '';
-    const option = <HTMLOptionElement>document.createElement('option');
-    option.innerHTML = '----------'; // this option forces the user to select their MIDI device, enabling the audiocontext (https://developers.google.com/web/updates/2017/09/autoplay-policy-changes#webaudio)
-    selectBox.appendChild(option)
-    for (let i = 0; i < midiInputPorts.length; i++) {
-        const inputPort = midiInputPorts[i];
-        const option = <HTMLOptionElement>document.createElement('option');
-        option.value = i.toString();
-        option.innerHTML = inputPort.name;
-        selectBox.appendChild(option);
+    constructor() {
+        navigator.requestMIDIAccess()
+            .then(
+                (midiAccess) => { this.requestMIDIAccessResolve(midiAccess) },
+                (exception) => this.requestMIDIAccessReject(exception));
     }
-    selectBox.selectedIndex = 0;
-}
 
-function selectMIDIInputPort() {
-    audioContext = new AudioContext();
-    oscillator = audioContext.createOscillator();
-    oscillator.type = "sawtooth";
-    oscillator.start(0);
-    selectedMidiInputPort = midiInputPorts[selectBox.selectedIndex - 1];
-    selectedMidiInputPort.onmidimessage = function (e: WebMidi.MIDIMessageEvent) {
+    /**
+     * This method gets called when MIDI access is granted. This function will initialize the whole application.
+     * @param midiAccess 
+     */
+    private requestMIDIAccessResolve(midiAccess: WebMidi.MIDIAccess): void {
+        const midiInputs: WebMidi.MIDIInputMap = midiAccess.inputs;
+        midiInputs.forEach((input: WebMidi.MIDIInput) => {
+            this.midiInputPorts.push(input);
+        });
+        this.updateMIDISelectBox();
+        this.selectMIDIDeviceBox.onchange = () => this.selectMIDIInputPort(); // the user must first interact with the site to allow an audioContext to be created, we achieve this by forcing the user to select a MIDI input device
+        this.selectBendRangeBox.onchange = () => this.setBendRange();
+    }
+
+    /**
+     * This method gets called when MIDI access is denied. The application is unusable.
+     * @param exception 
+     */
+    private requestMIDIAccessReject(exception: any): void {
+        alert(exception);
+    }
+
+    private setBendRange(): void {
+        this.bendRange = parseInt(this.selectBendRangeBox.value) * 128;
+    }
+    private selectMIDIInputPort(): void {
+        console.log(this.audioContext);
+        if (this.audioContext == null) {
+            this.audioContext = new AudioContext();
+            this.oscillator = this.audioContext.createOscillator();
+            this.oscillator.start(0);
+        }
+
+        this.selectedMIDIInputPort = this.midiInputPorts[this.selectMIDIDeviceBox.value];
+        console.log(this.selectMIDIDeviceBox.selectedIndex)
+        this.selectedMIDIInputPort.onmidimessage = (e) => this.processMIDIMessage(e);
+    }
+
+    private processMIDIMessage(e: WebMidi.MIDIMessageEvent) {
         const data = e.data; // data of the midi message
         const channel = data[0] & 0xF; // gets the MIDI channel: from 0 to 15
         const command = data[0] & ~0xF; // gets the command, from 
         switch (command) {
-            case 1: setModulation(data[2]); break;
-            case 0x80: noteOff(data[1], data[2]); break;
-            case 0x90: noteOn(data[1], data[2]); break;
-            case 0xE0: setBend(data[1], data[2]); break;
+            case 1: this.setModulation(data[2]); break;
+            case 0x80: this.noteOff(data[1], data[2]); break;
+            case 0x90: this.noteOn(data[1], data[2]); break;
+            case 0xE0: this.setBend(data[1], data[2]); break;
         }
     }
+
+
+    private noteOn(note: number, force: number): void {
+        this.oscillator.frequency.setTargetAtTime(this.FREQUENCIES[note], this.audioContext.currentTime, 0);
+        this.oscillator.connect(this.audioContext.destination);
+    }
+
+    private noteOff(note: number, force: number): void {
+        this.oscillator.disconnect(this.audioContext.destination);
+    }
+
+    private setBend(highNibble: number, lowNibble: number) {
+        const combination = (lowNibble << 7) | highNibble;
+        const bend = (combination - 8192) * (this.bendRange * 100 / 127) / 8192;
+        this.oscillator.detune.setValueAtTime(bend, this.audioContext.currentTime);
+    }
+
+    private setModulation(value: number) {
+        //this.gain.setValueAtTime(value * 100 / 127, this.audioContext.currentTime);
+    }
+
+
+    private updateMIDISelectBox(): void {
+        this.selectMIDIDeviceBox.innerHTML = '';
+        const option = <HTMLOptionElement>document.createElement('option');
+        option.innerHTML = '----------'; // this option forces the user to select their MIDI device, enabling the audiocontext (https://developers.google.com/web/updates/2017/09/autoplay-policy-changes#webaudio)
+        this.selectMIDIDeviceBox.appendChild(option)
+        for (let i = 0; i < this.midiInputPorts.length; i++) {
+            const inputPort = this.midiInputPorts[i];
+            const option = <HTMLOptionElement>document.createElement('option');
+            option.value = i.toString();
+            option.innerHTML = inputPort.name;
+            this.selectMIDIDeviceBox.appendChild(option);
+        }
+        this.selectMIDIDeviceBox.selectedIndex = 0;
+    }
+
+
 }
 
-function noteOn(note: number, force: number): void {
-    oscillator.frequency.setTargetAtTime(FREQUENCIES[note], audioContext.currentTime, 0);
-    oscillator.connect(audioContext.destination);
-}
-
-function noteOff(note: number, force: number): void {
-    oscillator.disconnect(audioContext.destination);
-}
-
-function setBend(highNibble: number, lowNibble: number) {
-    const combination = (lowNibble << 7) | highNibble;
-    const bend = (combination - 8192) * (256 * 100 / 127) / 8192;
-    oscillator.detune.setValueAtTime(bend, audioContext.currentTime);
-}
-
-function setModulation(value: number) {
-    gain.gain.setValueAtTime(value * 100 / 127, audioContext.currentTime);
-}
-
-export { };
+new VirtualPiano();
